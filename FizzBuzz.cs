af880cfa64a53805a1bb66a00f07609d9925bd36
Julia Grifiths Allison <griffithsallison0615@gmail.com>
2022-11-03 19:28:04 -0400

    finished fizzbuzz

---
+++ FizzBuzz.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    internal class FizzBuzz
+    {
+        public void Fizzbuzz(int x)
+        {
+            if ((x % 5 == 0) && (x % 3 == 0))
+            {
+                Console.WriteLine("FIZZBUZZ");
+            }
+            else if (x % 5 == 0)
+            {
+                Console.WriteLine("BUZZ");
+            }
+            else if (x % 3 == 0)
+            {
+                Console.WriteLine("FIZZ");
+            }
+        }
+    }
+}
